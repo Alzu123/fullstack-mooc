9d848e5c5e6b379e115e8ad37d10505e618bcf34
Neero Lius <neero@lius.fi>
2020-05-05 23:22:48 +0300

    Up to 1.14, rest of anecdotes

--- Osa 1/anekdootit/src/index.js
+++ Osa 1/anekdootit/src/index.js
@@ -1,6 +1,14 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = ({ text }) => (
+  <div>
+    <h1>
+      {text}
+    </h1>
+  </div>
+)
+
 const Button = ({ onClick, text }) => (
   <button onClick={onClick}>
     {text}
@@ -13,21 +21,37 @@ const Display = (props) => {
   }
   
   return (
-    <p>
-      {props.anecdotes[props.selected]}
-    </p>
+    <div>
+      <p>
+        Votes: {props.votes[props.selected]}
+      </p>
+      <p>
+        {props.anecdotes[props.selected]}
+      </p>
+    </div>
   )
 }
 
 const App = (props) => {
   const [selected, setSelected] = useState(-1)
+  const [votes, setVotes] = useState( Array(anecdotes.length).fill(0) )
 
   const updateSelection = () => (Math.floor(Math.random() * anecdotes.length))
+  const updateVotes = (votes, selected) => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+  const indexOfMostVotes = votes.indexOf(Math.max(...votes));
 
   return (
     <div>
+      <Header text={"Anecdote of the day"} />
       <Button onClick={() => setSelected(updateSelection)} text={"Roll for new anecdote"} />
-      <Display selected={selected} anecdotes={anecdotes} />
+      <Button onClick={() => updateVotes(votes, selected)} text={"Vote for this anecdote"} />
+      <Display selected={selected} anecdotes={anecdotes} votes={votes}/>
+      <Header text={"Anecdote with the most votes"} />
+      <Display selected={indexOfMostVotes} anecdotes={anecdotes} votes={votes}/>
     </div>
   )
 }
